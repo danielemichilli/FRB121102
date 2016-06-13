f08a28fe396b51d2f8bea772217d64cc3f095352
Daniele Michilli <danielemichilli@gmail.com>
2016-06-13 14:56:34 +0200

    Updated

--- launcher.sh
+++ launcher.sh
@@ -7,7 +7,7 @@ fi
 
 OBS=$1
 INDIR=`pwd`
-FIL=${OBS}_SAP000_B000_cDM051.00.fil #_SAP000_B000_cDM558.00.fil
+FIL=${OBS}_SAP000_B000_cDM558.00.fil
 if [ ! -e ${OBS} ]; do
   echo "Obervation to process must be present in this folder"
   exit
@@ -19,23 +19,21 @@ echo "  - Processing of $1 starting"
 TMP_NAME=`date -I`
 mkdir ${TMP_NAME}
 
-#/data1/Daniele/FRB121102/software/splitfil -o ${TMP_NAME}/${OBS} ${FIL}
+/data1/Daniele/FRB121102/software/splitfil -o ${TMP_NAME}/${OBS} ${FIL}
 
 for i in `seq 0 3`; do
-  sh /data1/Daniele/FRB121102/software/repeating_FRB.sh ${OBS} $i &
+  sh /data1/Daniele/FRB121102/software/repeating_FRB.sh ${OBS} $i > ${OBS}_${i}.log &
 done
 wait
 
 for i in `seq 4 8`; do
-  sh /data1/Daniele/FRB121102/software/repeating_FRB.sh ${OBS} $i &
+  sh /data1/Daniele/FRB121102/software/repeating_FRB.sh ${OBS} $i > ${OBS}_${i}.log &
 done
 wait
 
-exit
-
 python repeating_FRB_diagnostic.py ${WORKDIR} ${OBS}.sp ${FIL} ${WORKDIR}/output/diagnostic_plots
 
-rm -r TMP_NAME
+# rm -r TMP_NAME
 
 echo "  - Processing of $1 finished"
 echo "============================="
--- repeating_FRB.sh
+++ repeating_FRB.sh
@@ -18,15 +18,15 @@ OUTDIR=/data1/Daniele/FRB121102/test
 mkdir ${OUTDIR}/${OBS}
 
 
-FIL=${OBS}_SAP000_B000_cDM051.00.fil #_p00${CHUNK}.fil
+FIL=${OBS}_p00${CHUNK}.fil  #_SAP000_B000_cDM051.00.fil
 cp ${INDIR}/${FIL} .
 
 rfifind -o ${OBS} -filterbank -noweights -noscales -nooffsets -clip 6.0 -zerodm -time 1.0 -rfips ${FIL}
 
 
 # Durations from 0.000652 to 0.0978 s (downfactor 4 to 4*150)
-prepsubband -o d1_${OBS} -noweights -noscales -nooffsets -clip 6.0 -zerodm -runavg -numout 1200000 -lodm 50.0 -dmstep 0.1 -numdms 20 -nsub 1600 -downsamp 4 -mask ${OBS}_rfifind.mask ${FIL}
-#prepsubband -o d1_${OBS} -noweights -noscales -nooffsets -clip 6.0 -zerodm -runavg -numout ?? -lodm 554.0 -dmstep 0.005 -numdms 2000 -nsub 3200 -downsamp 4 -mask ${OBS}_rfifind.mask ${FIL}
+# prepsubband -o d1_${OBS} -noweights -noscales -nooffsets -clip 6.0 -zerodm -runavg -numout 1200000 -lodm 50.0 -dmstep 0.1 -numdms 20 -nsub 1600 -downsamp 4 -mask ${OBS}_rfifind.mask ${FIL}
+prepsubband -o d1_${OBS} -noweights -noscales -nooffsets -clip 6.0 -zerodm -runavg -numout 6250000 -lodm 554.0 -dmstep 0.005 -numdms 2000 -nsub 3200 -downsamp 4 -mask ${OBS}_rfifind.mask ${FIL}
 single_pulse_search.py -m 0.1 -p -t 8 -b *.dat
 
 cat *.singlepulse | head -n 1 > ${OBS}.sp
@@ -42,14 +42,18 @@ for i in `ls d1_*.dat`; do
   rm ${base_name}.inf
 done
 single_pulse_search.py -m 13.0 -p -t 8 -b *.dat
-#ATTENZIONE! Moltiplicare Downfactor per 128
-tail --lines=+2 -q  *.singlepulse >> ${OBS}.sp
 
-rm d*.dat d*.inf d*.singlepulse
+tail --lines=+2 -q  *.singlepulse > sp.tmp
+rm *.singlepulse
+awk '$NF=$NF*128' sp.tmp >> rm *.singlepulse
+rm sp.tmp
+
+rm *.dat *.inf
 
 mkdir output
 mv *_rfifind.ps output
 mv ${OBS}.sp output
+mkdir ${OUTDIR}/${OBS}/${CHUNK}
 cp output/* ${OUTDIR}/${OBS}/${CHUNK}
   
 cd ${OUTDIR}
